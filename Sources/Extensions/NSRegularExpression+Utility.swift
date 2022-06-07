ae64f92d883a67f3c316375831f2bc5c588fa6c1
Mikael Sundström <mikael@sundstrom.st>
2022-06-07 21:06:49 +0200

    Add groups

--- Sources/Extensions/NSRegularExpression+Utility.swift
+++ Sources/Extensions/NSRegularExpression+Utility.swift
@@ -2,6 +2,7 @@ import Foundation
 
 public struct RegexMatch {
     public let match: String
+    public let groups: [String]
     public let lineNumber: Int
 }
 
@@ -11,28 +12,38 @@ public extension NSRegularExpression {
         return firstMatch(in: string, options: [], range: range) != nil
     }
 
-    func match(inFile string: String) -> RegexMatch? {
+    func match(inFile string: String) -> [RegexMatch] {
         let range = NSRange(location: 0, length: string.utf16.count)
-        let potentialMatch = firstMatch(in: string, options: [], range: range)
+        let allMatches = matches(in: string, options: [], range: range)
 
-        guard let match = potentialMatch else {
-            return nil
+        var matches: [RegexMatch] = []
+        for match in allMatches {
+            let fullMatch = string[match.range.lowerBound...match.range.upperBound]
+
+            var subMatches = [String]()
+            for matchIndex in 0..<match.numberOfRanges {
+                let subMatch = match.range(at: matchIndex)
+//                let startIndex = string.index(string.startIndex, offsetBy: subMatch.lowerBound)
+//                let endIndex = string.index(string.startIndex, offsetBy: subMatch.upperBound)
+                let range = subMatch.lowerBound..<subMatch.upperBound
+                subMatches.append(string[range])
+            }
+
+            let beforeMatchString = string[0..<match.range.lowerBound]
+            let linesCount = beforeMatchString.reduce(into: 1) { (count, letter) in
+               if letter == "\n" {
+                  count += 1
+               }
+            }
+
+            matches.append(RegexMatch(
+                match: fullMatch.trimmingCharacters(in: .whitespacesAndNewlines),
+                groups: subMatches,
+                lineNumber: linesCount)
+            )
         }
 
-        let matchString = string[match.range.lowerBound...match.range.upperBound]
-
-        let beforeMatchString = string[0..<match.range.lowerBound]
-
-        let linesCount = beforeMatchString.reduce(into: 1) { (count, letter) in
-           if letter == "\n" {
-              count += 1
-           }
-        }
-
-        return RegexMatch(
-            match: matchString.trimmingCharacters(in: .whitespacesAndNewlines),
-            lineNumber: linesCount
-        )
+        return matches
     }
 }
 
